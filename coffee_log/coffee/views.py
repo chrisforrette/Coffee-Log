b4c3b38fbba89ec221bd1347654e769ac180c087
Chris Forrette <chris@chrisforrette.com>
2010-02-09 09:20:16 -0800

    Getting basic user stuff started, applying annotate to drink counts to reduce queries

--- coffee_log/coffee/views.py
+++ coffee_log/coffee/views.py
@@ -4,21 +4,25 @@ from django.db.models import Count
 from coffee_log.coffee.models import CoffeeLog, CoffeeDrink, CoffeePlace
 
 def index(request):
-        
-    coffee_logs = CoffeeLog.objects.all()
-    coffee_drinks = CoffeeDrink.objects.all()
     
+    # print dir(request.user)
+    # print request.user.get_all_permissions()
+    
+    coffee_logs = CoffeeLog.objects.all()
+    
+    # Get coffee drinks and counts
+    
+    drinks = CoffeeDrink.objects.all().annotate(log_count=Count('coffeelog'))
     drink_names = []
     drink_counts = []
     drink_pcts = []
-    total = 0
     
-    for drink in coffee_drinks:
-        count = CoffeeLog.objects.filter(coffee_drink=drink.pk).count()
-        if count > 0:
-            drink_names.append(drink)
-            drink_counts.append(count)
-            total += count
+    for drink in drinks:
+        if drink.log_count > 0:
+            drink_names.append(drink.name)
+            drink_counts.append(drink.log_count)
+    
+    total = sum(drink_counts)
     
     # Figure percents
     
@@ -33,8 +37,9 @@ def index(request):
     place_pcts = []
     
     for place in places:
-        place_names.append(place.name)
-        place_counts.append(place.log_count)
+        if place.log_count > 0:
+            place_names.append(place.name)
+            place_counts.append(place.log_count)
     
     total = sum(place_counts)
     
